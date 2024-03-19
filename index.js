2ae8daff307c64c145c136c38bd3aedcfacb7dd5
maximmft <mmaufront@gmail.com>
2024-03-19 10:56:21 +0100

    finished

---
+++ index.js
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+
+console.log(
+    chalk.cyan("Yann")
+);
+
+console.log(
+    chalk.grey("Aude")
+);
+
+console.log(
+    chalk.magenta("Brian")
+);
+
+console.log(
+    chalk.white("Tidus")
+);
